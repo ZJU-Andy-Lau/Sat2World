513a1d577df561766a9fb8bc633e922dfa321d0e
andylau <1173602856@qq.com>
2026-03-27 20:55:04 +0800

    resume

--- engine/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Engine exports."""
-
-from .trainer import Trainer, TrainerCfg
-
-__all__ = ["Trainer", "TrainerCfg"]
--- render/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Render package exports."""
-
-from .rpc_gaussian_renderer import RPCGaussianRenderer, RPCGaussianRendererCfg
-
-__all__ = ["RPCGaussianRenderer", "RPCGaussianRendererCfg"]
